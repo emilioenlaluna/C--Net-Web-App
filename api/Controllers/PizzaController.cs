4af2f320051cbe3f1127dc7f0148400f87fe1288
Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
2023-02-27 00:19:49 +0000

    first commit

---
+++ api/Controllers/PizzaController.cs
@@ -0,0 +1,51 @@
+using PizzaApp.Models;
+using PizzaApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace PizzaApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PizzaController:ControllerBase{
+    public PizzaController(){
+
+    }
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll()=>PizzaService.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id){
+        var pizza=PizzaService.Get(id);
+        if(pizza==null){
+            return NotFound();
+        }
+        return pizza;
+    }
+    [HttpPost]
+    public IActionResult Create(Pizza pizza){
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get),new {id=pizza.Id},pizza);
+    }
+    [HttpPut("{id}")]
+    public IActionResult Update(int id,Pizza pizza){
+        if(id!=pizza.Id)
+            return BadRequest();
+        
+        var existingPizza=PizzaService.Get(id);
+        if(existingPizza is null){
+            return NotFound();
+        }
+        PizzaService.Update(pizza);
+        return NoContent();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int id){
+        var pizza=PizzaService.Get(id);
+        if(pizza is null)
+            return NotFound();
+        PizzaService.Delete(id);
+        return NoContent();
+    }
+}
